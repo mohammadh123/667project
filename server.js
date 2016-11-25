0290fb19ee32bc7133314392e7297a84f81a1539
Mohammad <mhammoud@mail.sfsu.edu>
2016-11-25 14:11:30 -0800

    added chat to thehome page

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express    =    require('express');
 var app        =    express();
+//var server = require('http').createServer(app);
+users = [];
+connections = [];
 
 require('./router/main')(app);
 app.set('views',__dirname + '/views');
@@ -8,4 +11,40 @@ app.engine('html', require('ejs').renderFile);
 
 var server     =    app.listen(3000,function(){
 console.log("Express is running on port 3000");
+});
+
+app.get('/',function(req,res){
+        res.render('index.html')
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+	
+	//Disconnect
+	socket.on('disconnect', function(data){
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);	
+	});
+
+	//Send Message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	//New User
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	}
 });
